8b75501649f0f8e1791b5718f1d29c3fb044dc5c
John Vanek <jasvanek@gmail.com>
2026-01-05 09:48:16 -0600

    added ConvertNumber

---
+++ convertNumber/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    public static string ConvertNumber(int number)
+    {
+        string result;
+        if (number > 0)
+        {
+            result = "Positive";
+        }
+        else if (number < 0)
+        {
+            result = "Negative";
+        }
+        else
+        {
+            result = "Zero";
+        }
+        return result;
+    }
+
+    public static void DisplayResult(int number, string result)
+    {
+        Console.WriteLine($"Number {number} is {result}");
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        string convertedNumber = ConvertNumber(number);
+        DisplayResult(number, convertedNumber);
+    }
+}
