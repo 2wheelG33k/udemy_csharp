c4afbfaf1dc2c7cfc57c53ab090bb7b1a87a3a47
John Vanek <jasvanek@gmail.com>
2026-01-05 09:50:25 -0600

    Added EnhancedSwith

---
+++ enhanced Switch/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int input = 6;
+        string output = input switch
+        {
+            1 => "First",
+            2 => "Second",
+            3 => "Third",
+            _ => "Other"
+        };
+        
+        Console.WriteLine(output);
+    }
+}
