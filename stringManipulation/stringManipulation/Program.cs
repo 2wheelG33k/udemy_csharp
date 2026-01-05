709224166778b6bacab223e32bc11de1552eaccf
John Vanek <jasvanek@gmail.com>
2026-01-05 09:52:53 -0600

    Added stringManiplulator

---
+++ stringManipulation/stringManipulation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace stringManipulation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int employeeId = 20;
+        string companyName = "UTCLI";
+
+        Console.WriteLine("Hello, my employee ID is " + employeeId + " and my company name is " + companyName);
+        
+        Console.WriteLine("Hello, my employee ID is {0} and my company name is {1}", employeeId, companyName);
+
+        Console.WriteLine($"Hello, my employee ID is {employeeId} and my company name is {companyName}");
+    }
+}
