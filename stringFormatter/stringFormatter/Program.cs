120694ff005522c81796679a40b0498ca622cb59
John Vanek <jasvanek@gmail.com>
2026-01-05 09:52:10 -0600

    Added stringFormatter

---
+++ stringFormatter/stringFormatter/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace stringFormatter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a string to format:");
+        
+        string userInput = Console.ReadLine();
+        
+        string userUpper = userInput.ToUpper();
+        
+        Console.WriteLine(userUpper);
+
+        char[] stringArray = userInput.ToCharArray();
+        Array.Reverse(stringArray);
+        string userReversed = new string(stringArray);
+        
+        Console.WriteLine(userReversed);
+        
+        string userNoSpace = userInput.Replace(" ", "_");
+
+        Console.WriteLine(userNoSpace);
+
+        Console.WriteLine(userInput);
+    }
+}
