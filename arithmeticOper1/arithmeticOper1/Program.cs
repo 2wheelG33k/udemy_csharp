8c97c9b2f1e220fdf1187b53421196996d5ea61c
John Vanek <jasvanek@gmail.com>
2026-01-05 09:25:28 -0600

    Added ArithmeticOper1

---
+++ arithmeticOper1/arithmeticOper1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace arithmeticOper1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 8;
+        
+        int total = num1 + num2;
+        
+        Console.WriteLine($"{num1} + {num2} = {total}");
+
+        int count = 10;
+
+        count += 5;
+        
+        Console.WriteLine($"The value of count is {count}");
+    }
+}
