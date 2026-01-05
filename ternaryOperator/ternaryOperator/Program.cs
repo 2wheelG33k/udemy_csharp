5dbc20edc37c4a56028c1dee9948ed7ef139e7a3
John Vanek <jasvanek@gmail.com>
2026-01-05 09:53:37 -0600

    Added ternaryOperator

---
+++ ternaryOperator/ternaryOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ternaryOperator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nationality = "French";
+        bool isFrench = (nationality == "French" ? true : false);
+        Console.WriteLine(isFrench);
+
+        int years = 30;
+        string userStatus = (years < 18 ? "Minor" : "Adult");
+        Console.WriteLine(userStatus);
+    }
+}
